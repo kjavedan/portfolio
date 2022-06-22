b29020e5e8d529c55df53a99d64ade08da1f5cde
khaled javedan <khaledjavedan76@gmail.com>
2022-06-22 20:48:27 +0430

    working on the navbar

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,11 @@
 const burgerBtn = document.querySelector('.menu-btn__burger');
 const menuBtn = document.querySelector('.menu-btn');
 const navMenu = document.querySelector('.nav-menu');
+const test = document.querySelector('.test');
+
 
 menuBtn.addEventListener('click', ()=>{
     burgerBtn.classList.toggle('open');
     navMenu.classList.toggle('open');
+    test.classList.toggle('open');
 })
