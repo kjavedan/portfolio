136f69978c64cd1c1ed04eaf64017fa06c00eab0
khaled javedan <khaledjavedan76@gmail.com>
2022-06-23 09:30:43 +0430

    working on the navbar animation

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,11 +1,17 @@
 const burgerBtn = document.querySelector('.menu-btn__burger');
 const menuBtn = document.querySelector('.menu-btn');
 const navMenu = document.querySelector('.nav-menu');
-const test = document.querySelector('.test');
+const navMenuItem = document.querySelectorAll('.nav-menu__item');
+const rightDiv = document.querySelector('.right-div__animation');
+const leftDiv = document.querySelector('.left-div__animation');
 
 
 menuBtn.addEventListener('click', ()=>{
     burgerBtn.classList.toggle('open');
     navMenu.classList.toggle('open');
-    test.classList.toggle('open');
+    rightDiv.classList.toggle('open');
+    leftDiv.classList.toggle('open');
+    navMenuItem.forEach(item =>{
+        item.classList.toggle('open');
+    })
 })
