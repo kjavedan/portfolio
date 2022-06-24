eaf3d143717be569be6cb421f16201d4394c857d
khaled javedan <khaledjavedan76@gmail.com>
2022-06-24 18:55:22 +0430

    working on the landing page animation

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,7 +4,14 @@ const navMenu = document.querySelector('.nav-menu');
 const navMenuItem = document.querySelectorAll('.nav-menu__item');
 const rightDiv = document.querySelector('.right-div__animation');
 const leftDiv = document.querySelector('.left-div__animation');
+const myName = document.querySelector('.my-name');
+const myJob = document.querySelector('.my-job');
 
+window.addEventListener('DOMContentLoaded', ()=>{
+    myName.classList.add('open');
+    myJob.classList.add('open');
+    
+})
 
 menuBtn.addEventListener('click', ()=>{
     burgerBtn.classList.toggle('open');
