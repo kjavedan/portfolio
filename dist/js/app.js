46a9c29613c6257513a659ffe7ba2bf6ca2ba116
khaled javedan <khaledjavedan76@gmail.com>
2022-06-25 21:05:27 +0430

    adding some features to the about me section

--- dist/js/app.js
+++ dist/js/app.js
@@ -6,11 +6,20 @@ const rightDiv = document.querySelector('.right-div__animation');
 const leftDiv = document.querySelector('.left-div__animation');
 const myName = document.querySelector('.my-name');
 const myJob = document.querySelector('.my-job');
+const myTitle = document.querySelectorAll('.my-title');
 
 window.addEventListener('DOMContentLoaded', ()=>{
     myName.classList.add('open');
     myJob.classList.add('open');
     
+})
+window.addEventListener('scroll', ()=>{
+    const scrollingPosition = window.pageYOffset;
+    console.log(scrollingPosition);
+    if(scrollingPosition > 250){
+        aboutMe();
+    }
+  
 })
 
 menuBtn.addEventListener('click', ()=>{
@@ -22,3 +31,15 @@ menuBtn.addEventListener('click', ()=>{
         item.classList.toggle('open');
     })
 })
+
+
+// functions************************************
+
+// add .open to about me section when the the section position is reached;
+function aboutMe(){
+    myTitle.forEach(item =>{
+        console.log('added');
+        item.classList.add('open');
+    })
+    
+}
