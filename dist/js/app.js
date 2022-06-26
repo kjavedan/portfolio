aca117c0404f75e259da49b75d7353be81e9e166
khaled javedan <khaledjavedan76@gmail.com>
2022-06-26 15:09:02 +0430

    working on the how I can help section

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,7 @@
 // getting the sections 
 const aboutMe = document.getElementById('about-me');
-// console.log(aboutMe);
+const mySkills = document.getElementById('my-skills');
+console.log(mySkills);
 
 const burgerBtn = document.querySelector('.menu-btn__burger');
 const menuBtn = document.querySelector('.menu-btn');
@@ -15,6 +16,11 @@ const animationLine =document.querySelectorAll('.container__animation-line');
 const sectionSubtitle = document.querySelectorAll('.container__subtitle');
 const sectionText =document.querySelectorAll('.container__text');
 const sectionButton = document.querySelectorAll('.container__button');
+const icons = document.querySelector('.icons');
+const IconContainer = document.querySelectorAll('.icons-container');
+console.log(icons);
+
+
 window.addEventListener('DOMContentLoaded', ()=>{
     myName.classList.add('open');
     myJob.classList.add('open');
@@ -22,15 +28,22 @@ window.addEventListener('DOMContentLoaded', ()=>{
 })
 window.addEventListener('scroll', ()=>{
     const scrollingPosition = window.pageYOffset;
-    const aboutMePosition = aboutMe.getBoundingClientRect().height;
-    console.log(aboutMePosition);
-    if(scrollingPosition > aboutMePosition){
-        setAboutMe();
-        setSectionValues(animationLine);
-        setSectionValues(sectionSubtitle);
-        setSectionValues(sectionText);
-        setSectionValues(sectionButton);     
-    }
+    const aboutMePosition = aboutMe.getBoundingClientRect();
+    const mySkillPosition = mySkills.getBoundingClientRect();
+    // console.log(aboutMePosition);
+    // console.log(scrollingPosition);////////////
+    // if(aboutMePosition <= 100){
+    //     setAboutMe();
+    //     setSectionValues(animationLine);
+    //     setSectionValues(sectionSubtitle);
+    //     setSectionValues(sectionText);
+    //     setSectionValues(sectionButton); 
+       
+    // }
+    //  if(mySkillPosition <=100){
+    //     icons.classList.add('open');
+    //     setSectionValues(IconContainer);
+    // }
   
 })
 
@@ -50,14 +63,14 @@ menuBtn.addEventListener('click', ()=>{
 // add .open to about me section when the the section position is reached;
 function setAboutMe(){
     myTitle.forEach(item =>{
-        console.log('added');
+        // console.log('added');
         item.classList.add('open');
     })
     
 }
 // function to add the section list items
 function setSectionValues(items){
-    console.log(items);     
+    // console.log(items);     
     items.forEach(item =>{
         item.classList.add('open');
     })
