05ef8014732967524b3695aa9570f1b9f3bc7ded
khaled javedan <khaledjavedan76@gmail.com>
2022-07-20 15:27:27 +0430

    working on the animation of the project.html file

---
+++ dist/js/projects.js
@@ -0,0 +1,43 @@
+const changingText = document.querySelector('.changing-text')
+
+
+const myArray = [{
+    text : " I'm excited to show you my work :)",
+    color : "blue"
+},
+{
+    text: " Please let me know if you enjoy it",
+    color: "red"
+},
+{
+    text: " Nice to meet you!",
+    color: "yellow"
+}];
+
+
+let count = 0;
+
+function fadeInOut(){
+    console.log('executed');
+    changingText.innerHTML = "";
+    changingText.style.color = myArray[count].color;
+    const textLetter = myArray[count].text.split("")
+
+    for(let i=0; i < textLetter.length; i++){
+        
+        changingText.innerHTML += `<span class="animated-text">${textLetter[i]}</span>`
+        setTimeout(()=>{
+            document.querySelectorAll('.animated-text')[i].classList.add('fade-in')
+        }, 20 * i)
+        setTimeout(()=>{
+            setTimeout(()=>{
+                document.querySelectorAll('.animated-text')[i].classList.add('fade-out')
+            },20 * i)
+        },3500)
+    }
+    count++;
+    if(count == myArray.length) count = 0;
+}
+
+fadeInOut()
+setInterval(fadeInOut,5000)
