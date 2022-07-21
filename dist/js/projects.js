d32e0ae48a368703808e885395c34f1001f9ffd7
khaled javedan <khaledjavedan76@gmail.com>
2022-07-21 18:52:32 +0430

    working on displaying projects at 'projects.html' file

--- dist/js/projects.js
+++ dist/js/projects.js
@@ -1,5 +1,3 @@
-const changingText = document.querySelector('.changing-text')
-
 const myArray = [{
     text : " I'm excited to show you my work :)",
     color : "blue"
@@ -13,7 +11,56 @@ const myArray = [{
     color: "lightgreen"
 }];
 
+const projectsData = [
+    {
+        name : "Iron Brothers",
+        icon : "/dist/img/dumbbell.png",
+        backgroundImg : "/dist/img/IronBrothers.jpg",
+        category : "fitness",
+        type : "Website",
+        gihubLink : "",
+        liveLink : "",
+        onclickLink : ""
+    },
+    {
+        name : "Online Shop",
+        icon : "/dist/img/dumbbell.png",
+        backgroundImg : "/dist//img/ecommerce.jpg",
+        category : "Ecommerce",
+        type : "Website",
+        gihubLink : "",
+        liveLink : "",
+        onclickLink : ""
+    },
+    {
+        name : "Dream House",
+        icon : "/dist/img/dumbbell.png",
+        backgroundImg : "/dist//img/realEstate.jpg",
+        category : "Real Estate",
+        type : "Website",
+        gihubLink : "",
+        liveLink : "",
+        onclickLink : ""
+    },
+    {
+        name : "Cars",
+        icon : "/dist/img/dumbbell.png",
+        backgroundImg : "/dist//img/car.jpg",
+        category : "company",
+        type : "Website",
+        gihubLink : "",
+        liveLink : "",
+        onclickLink : ""
+    },
+]
 
+
+// variables
+const changingText = document.querySelector('.changing-text')
+const projectsContainer = document.querySelector('.projects');
+
+
+// landing page text animation function
 let count = 0;
 let wait = 20;
 function fadeInOut(){
@@ -41,3 +88,37 @@ function fadeInOut(){
 window.addEventListener('DOMContentLoaded', fadeInOut)
 // displaying the text on the main page each 5s
 setInterval(fadeInOut,5200)
+
+
+
+const projects = projectsData.map(item => {
+    return(
+        `<!-- start of project  -->
+    <div class="project-container open">
+        <div class="projects-item">
+            <a href="#" class="projects-item__link">${item.onclickLink}</a>
+            <img class="projects-item__image" src=${item.backgroundImg} alt="project-image">
+            <div class="projects-item__logo">
+                <img src=${item.icon} alt="logo">
+            </div>
+            <div class="projects-item__info">
+                <h1 class="project-title">${item.name}</h1>
+                <h4 class="project-subtitle">${item.type}</h4>
+                <h3 class="project-category">${item.category}</h3>
+            </div>
+            <div class="projects-item__btns">
+                <a class="project__btn" href=${item.liveLink}>
+                    <i class="fa-solid fa-eye"></i>
+                </a>
+                <a class="project__btn" href=${item.gihubLink}>
+                    <i class="fa fa-github" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- end of project -->`)
+}).join("")
+
+console.log(projects);
+
+projectsContainer.innerHTML = projects;
