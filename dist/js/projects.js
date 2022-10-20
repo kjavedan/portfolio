fa04871a5631217b518ae7baac3ce125003468e0
khaled javedan <khaledjavedan76@gmail.com>
2022-10-20 15:16:38 +0330

    styled project item btn

--- dist/js/projects.js
+++ dist/js/projects.js
@@ -159,15 +159,15 @@ function setProjectsItems(data){
                         <h1 class="project-title">${item.name}</h1>
                         <h4 class="project-subtitle">${item.type}</h4>
                         <h3 class="project-category">${item.category}</h3>
-                    </div>
-                    <div class="projects-item__btns">
-                        <a class="project__btn" href=${item.liveLink}>
-                            <i class="fa-solid fa-eye"></i>
-                        </a>
-                        <a class="project__btn" href=${item.gihubLink}>
-                            <i class="fa fa-github" aria-hidden="true"></i>
-                        </a>
-                    </div>
+                        </div>
+                        <div class="projects-item__btns">
+                            <a class="project__btn" href=${item.liveLink}>
+                                <i class="fa-solid fa-eye"></i>
+                            </a>
+                            <a class="project__btn" href=${item.gihubLink}>
+                                <i class="fa fa-github" aria-hidden="true"></i>
+                            </a>
+                        </div>
                 </a>
             </div>
         </div>
